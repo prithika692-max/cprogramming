621a973aefb708def2d18a7dd5cdab8c48fea037
prithika692-max <prithika692@gmail.com>
2026-02-04 09:37:34 +0530

    Finish the program

--- Attendance count O(n).c
+++ Attendance count O(n).c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int attendance[] = {1, 1, 1, 1, 1}; // 1 = present
+    int attendance[] = {1, 1, 1, 1, 1}; 
     int n = 5;
     int count = 0;
 
-    // Scan attendance list once
     for (int i = 0; i < n; i++) {
         if (attendance[i] == 1) {
             count++;
